23621b53f856469e9a16036e0d1e14a18785e468
Haenuki Sachi <sachihaenuki1@gmail.com>
2024-08-11 22:31:34 +0700

    Push Program

--- data.js
+++ data.js
@@ -1,229 +1,229 @@
 export let kanji = [
-   {
+  {
     hint: "Dislike",
     answer: "嫌い"
   },
-   {
+  {
     hint: "Sightseeing",
     answer: "観光"
   },
-    {
+  {
     hint: "Homework",
     answer: "宿題"
-  },  
+  },
   {
     hint: "Delicious",
     answer: "美味しい"
   },
-    {
+  {
     hint: "Ride",
     answer: "乗る"
   },
-     {
+  {
     hint: "Paint",
     answer: "塗る"
   },
-       {
+  {
     hint: "Stuffed Animal",
-    answer: "ぬいぐるみ"
+    answer: "縫い包み"
   },
   {
     hint: "Take Off Clothes",
     answer: "脱ぐ"
   },
-   {
+  {
     hint: "Crab",
     answer: "蟹"
   },
-   {
+  {
     hint: "Diary",
     answer: "日記"
   },
-    {
+  {
     hint: "Rainbow",
     answer: "虹"
   },
-       {
+  {
     hint: "Want",
     answer: "欲しい"
   },
-         {
+  {
     hint: "Strawberry",
     answer: "苺"
   },
-   {
+  {
     hint: "Peach",
     answer: "桃"
   },
-     {
+  {
     hint: "Movie",
     answer: "映画"
   },
-       {
+  {
     hint: "Actor",
     answer: "俳優"
   },
-        {
+  {
     hint: "Bag (Big Size)",
     answer: "鞄"
   },
-           {
+  {
     hint: "Bag (Small Size)",
     answer: "袋"
   },
-            {
+  {
     hint: "Thirsty",
-    answer: "渇いた"
+    answer: "渇く"
   },
-                {
+  {
     hint: "Throat",
     answer: "喉"
   },
-   {
+  {
     hint: "Hungry",
-    answer: "空いた"
+    answer: "空く"
   },
-    {
+  {
     hint: "Stomach",
     answer: "お腹"
   },
-     {
+  {
     hint: "Forest",
     answer: "森"
   },
-      {
+  {
     hint: "Impossible",
     answer: "無理"
   },
-       {
+  {
     hint: "To Protect",
     answer: "守る"
   },
-        {
+  {
     hint: "Magic",
     answer: "魔法"
   },
-         {
+  {
     hint: "Candy",
     answer: "飴"
-  },     
-   {
+  },
+  {
     hint: "Apple",
     answer: "林檎"
   },
-      {
-    hint: "coffee shop",
+  {
+    hint: "Coffee shop",
     answer: "喫茶店"
   },
-       {
+  {
     hint: "Next/Beside",
     answer: "隣"
   },
-         {
+  {
     hint: "Intersection",
     answer: "交差点"
   },
-            {
+  {
     hint: "Left Side",
     answer: "左側"
   },
-              {
+  {
     hint: "Bridge",
     answer: "橋"
   },
-   {
+  {
     hint: "Cross/To pass through",
     answer: "渡る"
   },
-    {
+  {
     hint: "Broken",
     answer: "壊れる"
   },
-       {
+  {
     hint: "Together",
     answer: "一緒"
   },
-          {
+  {
     hint: "Swim",
     answer: "泳ぐ"
   },
-          {
+  {
     hint: "Desk",
     answer: "机"
   },
-            {
+  {
     hint: "Carrot",
     answer: "人参"
   },
-               {
+  {
     hint: "Radish",
     answer: "大根"
   },
-                {
+  {
     hint: "Salt",
     answer: "塩"
   },
-                 {
+  {
     hint: "The day before tomorrow",
     answer: "一昨日"
   },
-                  {
+  {
     hint: "Bicycle",
     answer: "自転車"
   },
-                    {
+  {
     hint: "Airplane",
     answer: "飛行機"
   },
-                      {
+  {
     hint: "Travel",
     answer: "旅行"
   },
-                       {
+  {
     hint: "Key",
     answer: "鍵"
   },
-                       {
+  {
     hint: "Turtle",
     answer: "亀"
   },
-                        {
+  {
     hint: "Snake",
     answer: "蛇"
   },
-                         {
+  {
     hint: "Snow",
     answer: "雪"
   },
-                         {
+  {
     hint: "Joke",
     answer: "冗談"
   },
-                           {
+  {
     hint: "Ship",
     answer: "船"
   },
-                            {
+  {
     hint: "Luke Warm",
     answer: "温い"
   },
-{
+  {
     hint: "Play",
     answer: "遊ぶ"
   },
-   {
+  {
     hint: "Differ",
     answer: "違う"
   },
-    {
+  {
     hint: "To Ask (To do something)",
     answer: "頼む"
   },
-   {
+  {
     hint: "To Stop",
     answer: "止まる"
   },
-     {
+  {
     hint: "To Suck",
     answer: "吸う"
   },
@@ -231,206 +231,207 @@ export let kanji = [
     hint: "Answer (Verb)",
     answer: "答える"
   },
-   {
+  {
     hint: "Music",
     answer: "音楽"
   },
-    {
+  {
     hint: "Take (Picture)",
     answer: "撮る"
   },
-     {
+  {
     hint: "Cut",
     answer: "切る"
   },
-       {
+  {
     hint: "Send (Package or Message)",
     answer: "送る"
   },
-        {
+  {
     hint: "Police/Police Station",
     answer: "警察"
   },
-         {
+  {
     hint: "Teach/Tell",
     answer: "教える"
   },
-            {
+  {
     hint: "Study",
     answer: "習う"
   },
-             {
+  {
     hint: "To Get Married",
     answer: "結婚する"
   },
-              {
+  {
     hint: "Recommendation",
     answer: "お勧め"
   },
-                 {
+  {
     hint: "Divorce",
     answer: "離婚する"
   },
-                   {
+  {
     hint: "Delete/Erase/Turn off",
     answer: "消す"
   },
-                     {
+  {
     hint: "To Add/To Attach/Turn On",
     answer: "付ける"
   },
-                      {
+  {
     hint: "Hurry",
     answer: "急ぐ"
   },
-                        {
+  {
     hint: "Wait",
     answer: "待つ"
   },
-                          {
+  {
     hint: "Use",
     answer: "使う"
   },
-                         {
+  {
     hint: "Put",
     answer: "置く"
   },
-                            {
+  {
     hint: "Live",
     answer: "住む"
   },
-                              {
+  {
     hint: "Phone Call",
     answer: "電話する"
   },
-                               {
+  {
     hint: "Memorize",
     answer: "覚える"
   },
-   
-                               {
+
+  {
     hint: "Forget",
     answer: "忘れる"
   },
-   
-                               {
+
+  {
     hint: "Pay",
     answer: "払う"
   },
-                                {
+  {
     hint: "Return",
     answer: "返す"
   },
-                         {
+  {
     hint: "Worry",
     answer: "心配する"
   },
-   
-                         {
+
+  {
     hint: "Work Overtime",
-    answer: "残業します"
+    answer: "残業する"
   },
-                            {
+  {
     hint: "Change (Money)",
     answer: "お釣り"
   },
-                               {
+  {
     hint: "Dresser (Money)",
     answer: "箪笥"
   },
-                               {
+  {
     hint: "Cotton Swabs",
     answer: "綿棒"
   },
-                               {
+  {
     hint: "Ruler",
     answer: "定規"
   },
-                               {
+  {
     hint: "Scissors",
     answer: "鋏"
   },
-                               {
+  {
     hint: "Eraser",
     answer: "消しゴム"
   },
-                               {
+  {
     hint: "Meeting",
     answer: "会議"
   },
-                               {
+  {
     hint: "President (Company)",
     answer: "社長"
   },
-                               {
+  {
     hint: "Salary",
     answer: "給料"
   },
-                               {
+  {
     hint: "Leopard",
     answer: "豹"
   },
-                               {
+  {
     hint: "Hipopotamus",
     answer: "河馬"
   },
-                               {
+  {
     hint: "Many",
     answer: "沢山"
   },
-                                  {
+  {
     hint: "The Previous Day",
     answer: "前日"
   },
-                                  {
+  {
     hint: "Tall",
     answer: "長身"
   },
-                                    {
+  {
     hint: "Skilled",
     answer: "上手"
   },
-                                    {
+  {
     hint: "On the way from school",
-    answer: "下校"
+    answer: "下校する"
   },
-                                      {
+  {
     hint: "Southeast",
     answer: "東南"
   },
-                                       {
+  {
     hint: "Elementary School",
     answer: "小学生"
-  }, 
-                                       {
+  },
+  {
     hint: "Life",
     answer: "人生"
   },
-{
+  {
     hint: "Draft Beer",
     answer: "生ビール"
   },
-   {
+  {
     hint: "Boy",
     answer: "男子"
   },
-   {
+  {
     hint: "Girl",
     answer: "女子"
   },
-     {
+  {
     hint: "Factory",
     answer: "工場"
-  } ,
-        {
+  },
+  {
     hint: "Field Trip",
     answer: "見学"
   },
-{
+  {
     hint: "Wide",
     answer: "広い"
-},
-   {
+  },
+  {
     hint: "Onsen",
-    answer: "温泉"}
+    answer: "温泉"
+  }
 ]
